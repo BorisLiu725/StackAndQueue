ae4441d433706ebd19e512d3f3e12e2a16c63716
bosaidong2333 <40694639+WaterAndFreedom@users.noreply.github.com>
2018-11-11 13:09:35 +0800

    Update Queue.java
    给队列接口添加注释

--- Queue.java
+++ Queue.java
@@ -1,7 +1,12 @@
 public interface Queue<E> {
+	//得到队列的元素个数
 	int getSize();
+	//判断队列是否为空
 	boolean isEmpty();
+	//出队操作，返回出队元素
 	E dequeue();
+	//入队操作
 	void enqueue(E e);
+	//得到队首元素（注意：不删除）
 	E getFront();
 }
